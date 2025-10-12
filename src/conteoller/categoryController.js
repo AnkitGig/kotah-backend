1a70a77cf0e00a8fdb52d8380c14e2e0797c3f1b
AnkitGig <ankitchouhan1785@gmail.com>
2025-10-12 10:45:09 +0530

    Cateory Image Add A Couldnary

--- src/conteoller/categoryController.js
+++ src/conteoller/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require('../models/Category');
+const path = require('path');
 
 exports.getCategories = async (req, res) => {
   try {
@@ -10,15 +11,32 @@ exports.getCategories = async (req, res) => {
   }
 };
 
-// public - create category, only image and title required
 exports.createCategory = async (req, res) => {
   try {
     const { title, description } = req.body || {};
     const file = req.file;
     const imageFromBody = req.body && req.body.image;
-    const image = file ? `/uploads/${file.filename}` : imageFromBody;
+
+    let image;
+    if (file) {
+      try {
+        const uploaderPath = path.join(__dirname, '..', 'utils', 'customUploader.js');
+        const uploaderModule = require(uploaderPath);
+        const customUploader = uploaderModule && uploaderModule.default ? uploaderModule.default : uploaderModule;
+        const uploadedUrl = await customUploader({ file: req.file, folder: 'categories' });
+        if (uploadedUrl) image = uploadedUrl;
+        else image = req.file.path ? req.file.path.replace(/\\/g, '/') : `/uploads/${file.filename}`;
+      } catch (e) {
+        console.error('category image upload error', e);
+        image = req.file.path ? req.file.path.replace(/\\/g, '/') : `/uploads/${file.filename}`;
+      }
+    } else {
+      image = imageFromBody;
+    }
+
     if (!title || !image)
       return res.status(400).json({ message: 'title and image required' });
+
     const category = new Category({ title, image, description });
     await category.save();
     res.status(201).json(category);
