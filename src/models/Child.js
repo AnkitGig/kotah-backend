440dbceaecf15641bd6ceb830321201b915283af
AnkitGig <ankitchouhan1785@gmail.com>
2025-10-13 22:56:33 +0530

    Add a New Children Smart Phone Field

--- src/models/Child.js
+++ src/models/Child.js
@@ -4,6 +4,11 @@ const childSchema = new mongoose.Schema({
   parent: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   name: { type: String, required: true },
   age: { type: Number },
+  samartPhone: {
+    type: String,
+    enum: ["yes", "no"],
+    default: "yes"
+  },
   avatarUrl: { type: String },
   code: { type: String, required: true, unique: true }, // 6-digit unique code
   coins: { type: Number, default: 0 },
